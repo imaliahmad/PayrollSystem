e1c4de62bdf7759cb80bbee93f513b1fb816587b
MehrozQazi <mehrozqazi346@gmail.com>
2023-05-25 22:16:39 +0500

    Added Basic Config

---
+++ PS.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
