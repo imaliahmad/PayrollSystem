efaa839ea2c229b5ba314016df33411bb90a1241
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-05-26 01:03:01 +0500

    Implement  DAL Layer (#3)

---
+++ PS.DAL/LeaveDb.cs
@@ -0,0 +1,58 @@
+﻿using PS.BOL;
+using PS.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.DAL
+{
+    public interface ILeaveDb
+    {
+        IEnumerable<Leave> GetAll();
+        Leave GetById(int id);
+        bool Insert(Leave obj);
+        bool Update(Leave obj);
+        bool Delete(int id);
+    }
+    public class LeaveDb : ILeaveDb
+    {
+        private AppDbContext context;
+        public LeaveDb(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Leave> GetAll()
+        {
+            return context.Leave;
+        }
+        public Leave GetById(int id)
+        {
+            var obj = context.Leave.Find(id);
+            return obj;
+        }
+        public bool Insert(Leave obj)
+        {
+            context.Leave.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Update(Leave obj)
+        {
+            context.Leave.Update(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var obj = context.Leave.Find(id);
+            context.Leave.Remove(obj);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
+
+
+}
---
+++ PS.DAL/SalaryDb.cs
@@ -0,0 +1,56 @@
+﻿using PS.BOL;
+using PS.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.DAL
+{
+    public interface ISalaryDb
+    {
+        IEnumerable<Salary> GetAll();
+        Salary GetById(int id);
+        bool Insert(Salary obj);
+        bool Update(Salary obj);
+        bool Delete(int id);
+    }
+    public class SalaryDb : ISalaryDb
+    {
+        private AppDbContext context;
+        public SalaryDb(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Salary> GetAll()
+        {
+            return context.Salary;
+        }
+        public Salary GetById(int id)
+        {
+            var obj = context.Salary.Find(id);
+            return obj;
+        }
+        public bool Insert(Salary obj)
+        {
+            context.Salary.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Update(Salary obj)
+        {
+            context.Salary.Update(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var obj = context.Salary.Find(id);
+            context.Salary.Remove(obj);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
+
