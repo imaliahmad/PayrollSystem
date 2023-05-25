89b8783ffbf20cd2cba8422b09ba4b2a2f046471
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-05-26 01:26:05 +0500

    Implement BLL Layer (#4)

---
+++ PS.BLL/AttendenceBs.cs
@@ -0,0 +1,48 @@
+﻿using PS.BOL;
+using PS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.BLL
+{
+    public interface IAttendenceBs
+    {
+        IEnumerable<Attendence> GetAll();
+        Attendence GetById(int id);
+        bool Insert(Attendence obj);
+        bool Update(Attendence obj);
+        bool Delete(int id);
+    }
+    public class AttendenceBs : IAttendenceBs
+    {
+        private readonly IAttendenceDb objDb;
+        public AttendenceBs(IAttendenceDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<Attendence> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public Attendence GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(Attendence obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(Attendence obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
+
---
+++ PS.BLL/DepartmentBs.cs
@@ -0,0 +1,49 @@
+﻿using PS.BOL;
+using PS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.BLL
+{
+    public interface IDepartmentBs
+    {
+        IEnumerable<Department> GetAll();
+        Department GetById(int id);
+        bool Insert(Department obj);
+        bool Update(Department obj);
+        bool Delete(int id);
+    }
+    public class DepartmentBs : IDepartmentBs
+    {
+        private readonly IDepartmentDb objDb;
+        public DepartmentBs(IDepartmentDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public Department GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(Department obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(Department obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
+
+
