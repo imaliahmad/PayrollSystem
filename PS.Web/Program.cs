dc8cf2ecd496dc99e1bada90b9a66982894672d3
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-05-26 22:21:39 +0500

    Crud On Department (#5)

--- PS.Web/Program.cs
+++ PS.Web/Program.cs
@@ -1,12 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using PS.BLL;
+using PS.DAL;
+using PS.DAL.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 #region DALRegion
+builder.Services.AddTransient<IDepartmentDb, DepartmentDb>();
 #endregion
 
 #region BLLRegion
+builder.Services.AddTransient<IDepartmentBs, DepartmentBs>();
 #endregion
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(@"Server=MEHROZQAZI-PC\SQLEXPRESS;Database=PayrollSystemA;Trusted_Connection=True"));
 
 var app = builder.Build();
 
