198b3edf5b84fce7803ea08849f3841e244e68d5
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-05-27 21:19:12 +0500

    added Category, Product, Brands Models (#6)

---
+++ PS.BOL/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.BOL
+{
+    public class BaseEntity
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } // "32jh3j4-njk3jk4h3jkl4h-kjh34j-kh34jkh" 
+        public DateTime CreatedDate { get; set; } 
+        public Guid CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public bool IsActive { get; set; }
+        public BaseEntity()
+        {
+
+        }
+        // <= 120 
+        public BaseEntity(Guid id, DateTime createdDate, Guid createdBy, 
+                          DateTime modifiedDate, Guid modifiedBy, bool isActive)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+            CreatedBy = createdBy;
+            ModifiedDate = modifiedDate;
+            ModifiedBy = modifiedBy;
+            IsActive = isActive;
+        }
+    }
+}
---
+++ PS.BOL/Brands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.BOL
+{
+    [Table("Brands")]
+    public class Brands:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Navigations
+        public virtual IEnumerable<Product> Products { get; set; }
+        public Brands()
+        {
+
+        }
+
+        public Brands(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
